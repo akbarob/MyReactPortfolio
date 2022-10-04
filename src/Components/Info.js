59da31d44a960bf848a134cc58c79335081132cf
akbarob <akbarbadmus07@gmail.com>
2022-10-04 14:09:24 +0100

    chat app

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,189 +1,189 @@
-export const Info =[
-    {
-        id:0,
-        name:'confusion app',
-        image: "images/confus app.png",
-        website:"https://confusion-gules.vercel.app/",
-        skills:{
-            react:'React',
-            redux:'Redux',
-            bs:'Bootstrap',
-            rest:'REST Api',
-            css:'CSS',
-            firebase:"Firebase"
-        },
-        source: "https://github.com/akbarob/confusion"
+export const Info = [
+  {
+    id: 0,
+    name: "confusion app",
+    image: "images/confus app.png",
+    website: "https://confusion-gules.vercel.app/",
+    skills: {
+      react: "React",
+      redux: "Redux",
+      bs: "Bootstrap",
+      rest: "REST Api",
+      css: "CSS",
+      firebase: "Firebase",
+    },
+    source: "https://github.com/akbarob/confusion",
+  },
+  {
+    id: 1,
+    name: "wears app",
+    image: "images/wears 2.png",
+    website: "https://wears-app.vercel.app/",
+    skills: {
+      react: "React",
+      redux: "Redux",
+      bs: "Bootstrap",
+      rest: "REST Api",
+      css: "CSS",
+      heroku: "Heroku",
+    },
+    source: "https://github.com/akbarob/wears-app",
+  },
+  {
+    id: 6,
+    name: "portfolio app",
+    image: "images/port 2.png",
+    website: "https://akbarbadmus.vercel.app/",
+    skills: {
+      react: "React",
+      bs: "Bootstrap",
+      css: "CSS",
+    },
+    source: "https://github.com/akbarob/MyReactPortfolio",
+  },
+  {
+    id: 3,
+    name: "Wears Remake w/ Firebase",
+    image: "images/wears remake.png",
+    website: "https://wears.vercel.app/",
+    skills: {
+      react: "React",
+      redux: "Redux",
+      bs: "Bootstrap",
+      rest: "REST Api",
+      css: "CSS",
+      firebase: "firebase",
+    },
+    source: "https://github.com/akbarob/wears",
+  },
+  {
+    id: 3,
+    name: "Reacter Landing Page",
+    image: "images/reacter.png",
+    website: "https://reacter-akbarob.vercel.app//",
+    skills: {
+      react: "React",
+      bs: "Bootstrap",
+      css: "CSS",
+    },
+    source: "https://github.com/akbarob/reacter",
+  },
+  {
+    id: 4,
+    name: "Notify",
+    image: "images/notify.png",
+    website: "https://notify-steel.vercel.app/",
+    skills: {
+      typeScript: "Typescript",
+      react: "React",
+      css: "CSS",
+    },
+    source: "https://github.com/akbarob/Notify",
+  },
+  {
+    id: 5,
+    name: "Cloud-app",
+    image: "images/cloud.png",
+    website: "https://cloud-app-chi.vercel.app/",
+    skills: {
+      typeScript: "Typescript",
+      react: "React",
+      css: "Tailwind",
+    },
+    source: "https://github.com/akbarob/cloud-app",
+  },
+  {
+    id: 2,
+    name: "Chat-app",
+    image: "images/Chat.png",
+    website: "https://chat-appakbar.vercel.app/",
+    skills: {
+      typeScript: "Typescript",
+      react: "React",
+      css: "Tailwind",
+      firebase: "firebase",
+    },
+    source: "https://github.com/akbarob/chat-app",
+  },
+];
+export const ProfileLinks = [
+  {
+    id: 0,
+    github: "git",
+    linkedin: "link",
+    behance: "behance",
+  },
+];
 
-    },
-    {
-        id:1,
-        name:'wears app',
-        image: "images/wears 2.png",
-        website:"https://wears-app.vercel.app/",
-        skills:{
-            react:'React',
-            redux:'Redux',
-            bs:'Bootstrap',
-            rest:'REST Api',
-            css:'CSS',
-            heroku:'Heroku'
-        },
-        source: "https://github.com/akbarob/wears-app"
-    },
-    {
-        id:2,
-        name:'portfolio app',
-        image: "images/port 2.png",
-        website:"https://akbarbadmus.vercel.app/",
-        skills:{
-            react:'React',
-            bs:'Bootstrap',
-            css:'CSS'
-        
-        },
-        source: "https://github.com/akbarob/MyReactPortfolio"
-
-    },
-    {
-        id:3,
-        name:'Wears Remake w/ Firebase',
-        image: "images/wears remake.png",
-        website:"https://wears.vercel.app/",
-        skills:{
-            react:'React',
-            redux:'Redux',
-            bs:'Bootstrap',
-            rest:'REST Api',
-            css:'CSS',
-            firebase:'firebase'
-        
-        },
-        source: "https://github.com/akbarob/wears"
-
-    },
-    {
-        id:3,
-        name:'Reacter Landing Page',
-        image: "images/reacter.png",
-        website:"https://reacter-akbarob.vercel.app//",
-        skills:{
-            react:'React',
-            bs:'Bootstrap',
-            css:'CSS',
-        
-        },
-        source: "https://github.com/akbarob/reacter"
-
-    },
-    {
-        id:4,
-        name:'Notify',
-        image: "images/notify.png",
-        website:"https://notify-steel.vercel.app/",
-        skills:{
-            typeScript: 'Typescript',
-            react:'React',
-            css:'CSS',
-        
-        },
-        source: "https://github.com/akbarob/Notify"
-
-    },
-    {
-        id:5,
-        name:'Cloud-app',
-        image: "images/cloud.png",
-        website:"https://cloud-app-chi.vercel.app/",
-        skills:{
-            typeScript: 'Typescript',
-            react:'React',
-            css:'Tailwind',
-        
-        },
-        source: "https://github.com/akbarob/cloud-app"
-
-    },
-]
-export const ProfileLinks=[
-       {
-            id:0,
-            github:'git',
-            linkedin:'link',
-            behance:'behance',
-        }
-    
-]
-
-export const Skills =[
-    {
-        id:0,
-        skill:'HTML',
-        img:"https://cdn.worldvectorlogo.com/logos/html-1.svg"
-    },
-    {
-        id:1,
-        skill:'CSS',
-        img:'https://cdn.worldvectorlogo.com/logos/css-3.svg'
-    },
-    {
-        id:2,
-        skill:'Javascript',
-        img:'https://cdn.worldvectorlogo.com/logos/logo-javascript.svg'
-    },
-    {
-        id:3,
-        skill:'Typescript',
-        img:'https://cdn.worldvectorlogo.com/logos/typescript.svg'
-    },
-    {
-        id:4,
-        skill:'React',
-        img:'https://cdn.worldvectorlogo.com/logos/react-2.svg'
-    },
-    {
-        id:5,
-        skill:'Tailwindcss',
-        img:'https://cdn.worldvectorlogo.com/logos/tailwindcss.svg'
-    },
-    {
-        id:6,
-        skill:'Bootstrap',
-        img:'https://cdn.worldvectorlogo.com/logos/bootstrap-5-1.svg'
-    },
-    {
-        id:7,
-        skill:'Node JS',
-        img:'https://cdn.worldvectorlogo.com/logos/nodejs.svg'
-    },
-    {
-        id:8,
-        skill:'Github',
-        img:'https://cdn.worldvectorlogo.com/logos/github-icon-1.svg'
-    },
-    {
-        id:9,
-        skill:'Redux',
-        img:'https://cdn.worldvectorlogo.com/logos/redux.svg'
-    },
-    {
-        id:10,
-        skill:'Firebase',
-        img:'https://cdn.worldvectorlogo.com/logos/firebase-2.svg'
-    },
-    {
-        id:11,
-        skill:'MongoDB',
-        img:'https://cdn.worldvectorlogo.com/logos/mongodb-icon-1.svg'
-    },
-    {
-        id:12,
-        skill:'Express JS',
-        img:'https://cdn.worldvectorlogo.com/logos/express-109.svg'
-    },
-    {
-        id:13,
-        skill:'Passport',
-        img:'https://cdn.worldvectorlogo.com/logos/passport.svg'
-    },
-    
-]
+export const Skills = [
+  {
+    id: 0,
+    skill: "HTML",
+    img: "https://cdn.worldvectorlogo.com/logos/html-1.svg",
+  },
+  {
+    id: 1,
+    skill: "CSS",
+    img: "https://cdn.worldvectorlogo.com/logos/css-3.svg",
+  },
+  {
+    id: 2,
+    skill: "Javascript",
+    img: "https://cdn.worldvectorlogo.com/logos/logo-javascript.svg",
+  },
+  {
+    id: 3,
+    skill: "Typescript",
+    img: "https://cdn.worldvectorlogo.com/logos/typescript.svg",
+  },
+  {
+    id: 4,
+    skill: "React",
+    img: "https://cdn.worldvectorlogo.com/logos/react-2.svg",
+  },
+  {
+    id: 5,
+    skill: "Tailwindcss",
+    img: "https://cdn.worldvectorlogo.com/logos/tailwindcss.svg",
+  },
+  {
+    id: 6,
+    skill: "Bootstrap",
+    img: "https://cdn.worldvectorlogo.com/logos/bootstrap-5-1.svg",
+  },
+  {
+    id: 7,
+    skill: "Node JS",
+    img: "https://cdn.worldvectorlogo.com/logos/nodejs.svg",
+  },
+  {
+    id: 8,
+    skill: "Github",
+    img: "https://cdn.worldvectorlogo.com/logos/github-icon-1.svg",
+  },
+  {
+    id: 9,
+    skill: "Redux",
+    img: "https://cdn.worldvectorlogo.com/logos/redux.svg",
+  },
+  {
+    id: 10,
+    skill: "Firebase",
+    img: "https://cdn.worldvectorlogo.com/logos/firebase-2.svg",
+  },
+  {
+    id: 11,
+    skill: "MongoDB",
+    img: "https://cdn.worldvectorlogo.com/logos/mongodb-icon-1.svg",
+  },
+  {
+    id: 12,
+    skill: "Express JS",
+    img: "https://cdn.worldvectorlogo.com/logos/express-109.svg",
+  },
+  {
+    id: 13,
+    skill: "Passport",
+    img: "https://cdn.worldvectorlogo.com/logos/passport.svg",
+  },
+];
